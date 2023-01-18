8c10d559d7283b0500c9abd4af73c55b111fb7e2
gitdrive5 <121691578+gitdrive5@users.noreply.github.com>
2023-01-18 16:53:53 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,15 +8,15 @@ API_HASH = os.environ.get("API_HASH", "")
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
 
-FORCE_SUB = os.environ.get("FORCE_SUB", "") 
+FORCE_SUB = os.environ.get("FORCE_SUB", "tgnvsbot") 
 
 DB_NAME = os.environ.get("DB_NAME","")     
 
 DB_URL = os.environ.get("DB_URL","")
  
-FLOOD = int(os.environ.get("FLOOD", "10"))
+FLOOD = int(os.environ.get("FLOOD", "600"))
 
-START_PIC = os.environ.get("START_PIC", "")
+START_PIC = os.environ.get("START_PIC", "https://graph.org/file/df475b3dd8fea1634db46.png")
 
 ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '').split()]
 
