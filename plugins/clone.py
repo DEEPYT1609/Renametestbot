afd6c67c37a73865f4d3dff3eedbc3a5c712fd8b
CODE-BUDDY <111114081+BUDDY1609@users.noreply.github.com>
2023-05-05 14:55:21 +0530

    Create clone.py

---
+++ plugins/clone.py
@@ -0,0 +1,38 @@
+from asyncio import sleep
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, ForceReply, CallbackQuery
+from pyrogram.errors import FloodWait
+import humanize
+import random
+from helper.txt import mr
+from helper.database import db
+from config import START_PIC, FLOOD, ADMIN
+from config import API_ID, API_HASH, BOT_TOKEN, FORCE_SUB, PORT
+import os
+import re
+import asyncio
+import time
+from pyrogram import *
+from pyrogram.types import *
+
+
+
+@Client.on_message(filters.command('clone') & filters.user(ADMIN)))
+async def clone(bot: client, msg: message):
+    chat = msg.chat
+    text = await msg.reply("Usage:\n\n /clone token")
+    cmd = msg.command
+    phone = msg.command[1]
+    try:
+        await text.edit("Booting Your Client")
+                   # change this Directry according to ur repo
+        client = Client(":memory:", API_ID, API_HASH, bot_token=phone, plugins={"root": "plugins"})
+        await client.start()
+        idle()
+        user = await client.get_me()
+        await msg.reply(f"Your Client Has Been Successfully Started As @{user.username}! ✅ \n\n Now Add Your Bot And Assistant @Amala_music_assistant_1 To Your Chat!\n\nThanks for Cloning.")
+    except Exception as e:
+        await msg.reply(f"**ERROR:** `{str(e)}`\nPress /start to Start again.")
+        
+        Client.start()
+idle()
